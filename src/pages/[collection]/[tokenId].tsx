139987a26488967c82ba5243d8f855763eced8da
Timur Badretdinov <destinerx@gmail.com>
2022-10-08 15:13:04 +0400

    Add collection pages

---
+++ src/pages/[collection]/[tokenId].tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const CollectionAsset: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Azuki</title>
+        <meta
+          name="description"
+          content="Akuzi #4145"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+
+      <main>Azuki #4145</main>
+    </div>
+  );
+};
+
+export default CollectionAsset;
---
+++ src/pages/[collection]/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Collection: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Azuki</title>
+        <meta
+          name="description"
+          content="Akuzi"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+
+      <main>Azuki</main>
+    </div>
+  );
+};
+
+export default Collection;
