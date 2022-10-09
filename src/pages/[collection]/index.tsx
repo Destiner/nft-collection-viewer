31363c64c58712fd04cd4bd8ca1317c5dc9dfbdc
Timur Badretdinov <destinerx@gmail.com>
2022-10-09 11:21:14 +0400

    Render list of items on Collection page

--- src/pages/[collection]/index.tsx
+++ src/pages/[collection]/index.tsx
@@ -1,17 +1,43 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import AssetList from '@/components/collection/AssetList';
 import Hero from '@/components/collection/Hero';
-import { getDescription, getTitle } from '@/utils/collections';
+import Service, { AssetPreview } from '@/services/center';
+import { getAddress, getDescription, getTitle } from '@/utils/collections';
+import { ChainId } from '@/utils/chains';
+
+const key = process.env.NEXT_PUBLIC_CENTER_KEY || '';
+const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '1') as ChainId;
 
 const Collection: NextPage = () => {
   const router = useRouter();
+  const service = new Service(key, chainId);
 
   const collectionSlug = router.query.collection as string;
 
+  const [assets, setAssets] = useState<AssetPreview[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const address = getAddress(collectionSlug);
   const title = getTitle(collectionSlug);
   const description = getDescription(collectionSlug);
+
+  useEffect(() => {
+    fetchAssets();
+  }, []);
+
+  async function fetchAssets(): Promise<void> {
+    if (!address) {
+      return;
+    }
+    setLoading(true);
+    const assets = await service.getItems(address, 0);
+    setAssets(assets);
+    setLoading(false);
+  }
 
   return (
     <div>
@@ -29,6 +55,10 @@ const Collection: NextPage = () => {
 
       <main>
         <Hero id={collectionSlug} />
+        <AssetList
+          collection={collectionSlug}
+          items={assets}
+        />
       </main>
     </div>
   );
