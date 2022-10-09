6ee005a8b58f82c10e7ed02fd69e05a1f36a3135
Timur Badretdinov <destinerx@gmail.com>
2022-10-09 15:51:08 +0400

    Add cypress test todos

--- cypress/e2e/index.test.ts
+++ cypress/e2e/index.test.ts
@@ -4,4 +4,7 @@ describe('Index page', () => {
   it('redirect to default collection', () => {
     cy.url().should('contain', '/azuki');
   });
+
+  // TODO invalid collection redirects to 404
+  // TODO invalid token id redirects to 404
 });
