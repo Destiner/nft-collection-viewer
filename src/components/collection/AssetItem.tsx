cfbc5e250b7d37dcbcec44b171de024d57f3e7d0
Timur Badretdinov <destinerx@gmail.com>
2022-10-09 11:20:50 +0400

    AssetItem component implementation

--- src/components/collection/AssetItem.tsx
+++ src/components/collection/AssetItem.tsx
@@ -1,3 +1,55 @@
-const AssetItem = () => {};
+import Link from 'next/link';
+
+import { AssetPreview } from '@/services/center';
+
+interface Props {
+  collection: string;
+  data: AssetPreview;
+}
+
+const AssetItem = ({ collection, data }: Props) => {
+  return (
+    <Link
+      href={{
+        pathname: '/[collection]/[tokenId]',
+        query: { collection, tokenId: data.token_id },
+      }}
+    >
+      <div className="card">
+        <img src={data.metadata.image} />
+        <div className="label">{data.metadata.name}</div>
+        <style jsx>{`
+          .card {
+            display: flex;
+            flex-direction: column;
+            border: 1px solid var(--color-border-primary);
+            border-radius: 16px;
+            cursor: pointer;
+            transition: all 0.25s ease-in-out;
+          }
+
+          .card:hover {
+            border-color: var(--color-accent-primary);
+          }
+
+          img {
+            width: 240px;
+            height: 240px;
+            border-radius: 15px 15px 0 0;
+          }
+
+          .label {
+            margin: 4px;
+            font-size: 16px;
+            color: var(--color-text-primary);
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+        `}</style>
+      </div>
+    </Link>
+  );
+};
 
 export default AssetItem;
