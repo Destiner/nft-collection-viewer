0e06fcd678328b9e0b269dfd2b606d24cc21cb50
Timur Badretdinov <destinerx@gmail.com>
2022-10-09 11:55:11 +0400

    Add link to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
     <header>
-      NFT Collection Viewer
+      <Link href={{ pathname: '/' }}>
+        <span className="brand">NFT Collection Viewer</span>
+      </Link>
       <style jsx>{`
         header {
           height: 48px;
           border-top: 4px solid var(--color-accent-primary);
-          color: var(--color-text-primary);
           display: flex;
           align-items: center;
           padding: 0 32px;
           font-weight: 900;
           font-size: 20px;
+        }
+
+        .brand {
+          color: var(--color-text-primary);
         }
       `}</style>
     </header>
